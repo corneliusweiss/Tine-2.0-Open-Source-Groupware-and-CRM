a1219fecc714450ea383fab505eac64f5ce6f5c3
Philipp Schüle <p.schuele@metaways.de>
2009-01-09 15:41:30 +0000

    merged changes from trunk rev 6183-6192 into the branch (csv export for timesheets and addressbook and a small ie7 layout fix)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2009-02@6193 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Export/Csv.php
+++ tine20/Timetracker/Export/Csv.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id$
+ * @version     $Id:Csv.php 6182 2009-01-09 11:51:41Z p.schuele@metaways.de $
  * 
  */
 
@@ -26,45 +26,30 @@ class Timetracker_Export_Csv extends Tinebase_Export_Csv
      * @param Timetracker_Model_TimesheetFilter $_filter
      * @return string filename
      * 
-     * @todo resolve timeaccount and account
      * @todo add specific export values
      * @todo add ods (open office spreadsheet) creation
-     * @todo save in special download path
-     * @todo perhaps we can make this more generic (move it to Tinebase_Export_Csv)
      */
     public function exportTimesheets($_filter) {
-        
-        $filename = date('Y-m-d') . '_timesheet_export.csv';
         
         $timesheets = Timetracker_Controller_Timesheet::getInstance()->search($_filter);
         if (count($timesheets) < 1) {
             throw new Timetracker_Exception_NotFound('No Timesheets found.');
         }
+
+        // resolve timeaccounts
+        $timeaccountIds = $timesheets->timeaccount_id;
+        $timeaccounts = Timetracker_Controller_Timeaccount::getInstance()->getMultiple(array_unique(array_values($timeaccountIds)));
         
-        // to ensure the order of fields we need to sort it ourself!
-        $fields = array();
-        $skipFields = array(
-        );
-        foreach ($timesheets[0] as $fieldName => $value) {
-            if (! in_array($fieldName, $skipFields)) {
-                $fields[] = $fieldName;
-            }
-        }
+        // resolve accounts
+        $accountIds = $timesheets->account_id;
+        $accounts = Tinebase_User::getInstance()->getMultiple(array_unique(array_values($accountIds)));
         
-        $filehandle = fopen($filename, 'w');
-        
-        self::fputcsv($filehandle, $fields);
-        
-        // fill file with records
         foreach ($timesheets as $timesheet) {
-            $timesheetArray = array();
-            foreach ($fields as $fieldName) {
-                $timesheetArray[] = $timesheet->$fieldName;
-            }
-            self::fputcsv($filehandle, $timesheetArray);
+            $timesheet->timeaccount_id = $timeaccounts[$timeaccounts->getIndexById($timesheet->timeaccount_id)]->title;
+            $timesheet->account_id = $accounts[$accounts->getIndexById($timesheet->account_id)]->accountDisplayName;
         }
-        
-        fclose($filehandle);
+                
+        $filename = parent::exportRecords($timesheets);
         
         return $filename;
     }
---
+++ tine20/Tinebase/js/widgets/grid/ExportButton.js
@@ -0,0 +1,77 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.namespace('Tine.widgets', 'Tine.widgets.grid');
+
+/**
+ * @class Tine.widgets.grid.ExportButton
+ * @extends Ext.Button
+ * <p>export button</p>
+ * @constructor
+ */
+Tine.widgets.grid.ExportButton = function(config) {
+    config = config || {};
+    Ext.apply(this, config);
+    
+    Tine.widgets.grid.ExportButton.superclass.constructor.call(this);
+};
+
+Ext.extend(Tine.widgets.grid.ExportButton, Ext.Button, {	
+    /**
+     * @cfg {String} icon class
+     */
+    iconCls: 'action_export',
+    /**
+     * @cfg {String} format of export (default: csv)
+     */
+    format: 'csv',
+    /**
+     * @cfg {String} export function (for example: Timetracker.exportTimesheets)
+     */
+    exportFunction: null,
+    /**
+     * @cfg {Object} the filter toolbar with filter settings
+     */
+    filterToolbar: null,
+    /**
+     * @cfg {String} application tree id
+     */
+    appTreeId: null,
+    
+    /**
+     * do export
+     */
+    doExport: function() {    	
+    	var filterSettings = this.filterToolbar.getValue();
+        
+        // add container to filter
+    	if (this.appTreeId) {
+            var nodeAttributes = Ext.getCmp(this.appTreeId).getSelectionModel().getSelectedNode().attributes || {};
+            filterSettings.push(
+                {field: 'containerType', operator: 'equals', value: nodeAttributes.containerType ? nodeAttributes.containerType : 'all' },
+                {field: 'container',     operator: 'equals', value: nodeAttributes.container ? nodeAttributes.container.id : null       },
+                {field: 'owner',         operator: 'equals', value: nodeAttributes.owner ? nodeAttributes.owner.accountId : null        }
+            );
+    	}
+
+        //console.log(filterSettings);
+    	Tine.Tinebase.common.openWindow('exportWindow', 'index.php?method=' + 
+            this.exportFunction + '&_format=' + this.format + '&_filter=' + Ext.util.JSON.encode(filterSettings), 200, 150);
+    },
+    
+    /**
+     * @private
+     * 
+     * @todo add on click handler -> call export function with grid selected ids
+     */
+    handler: function() { 	
+        this.doExport();
+    }
+});
