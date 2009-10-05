319c268784becb5fb0f3f58c9d42772bfadd97ed
Philipp Schüle <p.schuele@metaways.de>
2009-10-05 10:05:21 +0000

    merged voipmanager changes from trunk
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2009-11-ms1@10747 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Setup/Update/Release2.php
+++ tine20/Voipmanager/Setup/Update/Release2.php
@@ -12,8 +12,9 @@
 class Voipmanager_Setup_Update_Release2 extends Setup_Update_Abstract
 {
     /**
-     * change all fields which store account ids from integer to string
-     * 
+     * rename username to defaultuser
+     * add auto to dtmfmode enum
+     * add column regserver, useragent and lastms
      */
     public function update_0()
     {
@@ -66,5 +67,245 @@ class Voipmanager_Setup_Update_Release2 extends Setup_Update_Abstract
         $this->_backend->addCol('asterisk_sip_peers', $declaration);
         
         $this->setApplicationVersion('Voipmanager', '2.1');
+    }    
+    
+    /**
+     * add the asterisk_redirects table
+     */    
+    public function update_1()
+    {
+        $tableDefinition = '        
+            <table>
+                <name>asterisk_redirects</name>
+                <engine>InnoDB</engine>
+                <charset>utf8</charset>
+                <version>1</version>
+                <declaration>
+                    <field>
+                        <name>id</name>
+                        <type>text</type>
+                        <length>40</length>
+                        <notnull>true</notnull>
+                    </field>
+                    <field>
+                        <name>sippeer_id</name>
+                        <type>text</type>
+                        <length>40</length>
+                        <notnull>true</notnull>
+                    </field>
+                    <field>
+                        <name>cfi_mode</name>
+                        <type>enum</type>
+                        <value>off</value>
+                        <value>number</value>
+                        <value>voicemail</value>
+                        <notnull>true</notnull>
+                    </field>
+                    <field>
+                        <name>cfi_number</name>
+                        <type>text</type>
+                        <length>80</length>
+                    </field>
+                    <field>
+                        <name>cfb_mode</name>
+                        <type>enum</type>
+                        <value>off</value>
+                        <value>number</value>
+                        <value>voicemail</value>
+                        <notnull>true</notnull>
+                    </field>
+                    <field>
+                        <name>cfb_number</name>
+                        <type>text</type>
+                        <length>80</length>
+                    </field>
+                    <field>
+                        <name>cfd_mode</name>
+                        <type>enum</type>
+                        <value>off</value>
+                        <value>number</value>
+                        <value>voicemail</value>
+                        <notnull>true</notnull>
+                    </field>
+                    <field>
+                        <name>cfd_number</name>
+                        <type>text</type>
+                        <length>80</length>
+                    </field>
+                    <field>
+                        <name>cfd_time</name>
+                        <type>integer</type>
+                        <length>11</length>
+                    </field>                                
+                    <index>
+                        <name>id</name>
+                        <primary>true</primary>
+                        <field>
+                            <name>id</name>
+                        </field>
+                    </index>
+                    <index>
+                        <name>asterisk_redirects::sippeer_id--asterisk_sip_peers::id</name>
+                        <field>
+                            <name>sippeer_id</name>
+                        </field>
+                        <foreign>true</foreign>
+                        <reference>
+                            <table>asterisk_sip_peers</table>
+                            <field>id</field>
+                        </reference>
+                    </index>   
+                </declaration>
+            </table>
+        ';
+
+        $table = Setup_Backend_Schema_Table_Factory::factory('String', $tableDefinition); 
+        $this->_backend->createTable($table);        
+
+        $this->setApplicationVersion('Voipmanager', '2.2');
+    }
+           
+    /**
+     * rename context to context_id and add foreign key for context in voicemail table
+     * add auto to dtmfmode enum
+     * add column regserver, useragent and lastms
+     */
+    public function update_2()
+    {        
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>context_id</name>
+                <type>text</type>
+                <length>40</length>
+                <notnull>true</notnull>
+            </field>');
+        $this->_backend->addCol('asterisk_voicemail', $declaration);
+        
+        $select = $this->_db->select()
+            ->distinct()
+            ->from(SQL_TABLE_PREFIX . 'asterisk_context', array('id', 'name'));
+        $contextes = $this->_db->fetchAll($select);
+        
+        foreach($contextes as $context) {
+            $this->_db->update(
+                SQL_TABLE_PREFIX . 'asterisk_voicemail', 
+                array('context_id' => $context['id']), 
+                $this->_db->quoteInto($this->_db->quoteIdentifier('context') .' = ?', $context['name'])
+            );
+        }
+        
+        $declaration = new Setup_Backend_Schema_Index_Xml('
+            <index>
+                <name>asterisk_voicemail::context_id--asterisk_context::id</name>
+                <field>
+                    <name>context_id</name>
+                </field>
+                <foreign>true</foreign>
+                <reference>
+                    <table>asterisk_context</table>
+                    <field>id</field>
+                </reference>
+            </index>');
+        $this->_backend->addForeignKey('asterisk_voicemail', $declaration);       
+        
+        $this->_backend->dropIndex('asterisk_voicemail', 'mailbox-context');
+        $this->_backend->dropCol('asterisk_voicemail', 'context');
+        
+        $this->setApplicationVersion('Voipmanager', '2.3');
+    }    
+
+    /**
+     * rename context to context_id and add foreign key for context
+     * add auto to dtmfmode enum
+     * add column regserver, useragent and lastms
+     */
+    public function update_3()
+    {        
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>members</name>
+                <type>integer</type>
+            </field>');
+        $this->_backend->addCol('asterisk_meetme', $declaration);
+
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>starttime</name>
+                <type>datetime</type>
+            </field>');
+        $this->_backend->addCol('asterisk_meetme', $declaration);
+
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>endtime</name>
+                <type>datetime</type>
+            </field>');
+        $this->_backend->addCol('asterisk_meetme', $declaration);
+        
+        $this->setApplicationVersion('Voipmanager', '2.4');
+    }
+        
+    /**
+     * make lastms a signed integer
+     */
+    public function update_4()
+    {        
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>lastms</name>
+                <type>integer</type>
+                <unsigned>false</unsigned>
+                <length>11</length>
+                <notnull>false</notnull>
+            </field>');
+        $this->_backend->alterCol('asterisk_sip_peers', $declaration);
+        
+        $this->setApplicationVersion('Voipmanager', '2.5');
+    }
+        
+    /**
+     * rename context to context_id and add foreign key for context in sippeers table
+     */
+    public function update_5()
+    {        
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>context_id</name>
+                <type>text</type>
+                <length>40</length>
+                <notnull>true</notnull>
+            </field>');
+        $this->_backend->addCol('asterisk_sip_peers', $declaration);
+        
+        $select = $this->_db->select()
+            ->distinct()
+            ->from(SQL_TABLE_PREFIX . 'asterisk_context', array('id', 'name'));
+        $contextes = $this->_db->fetchAll($select);
+        
+        foreach($contextes as $context) {
+            $this->_db->update(
+                SQL_TABLE_PREFIX . 'asterisk_sip_peers', 
+                array('context_id' => $context['id']), 
+                $this->_db->quoteInto($this->_db->quoteIdentifier('context') .' = ?', $context['name'])
+            );
+        }
+        
+        $declaration = new Setup_Backend_Schema_Index_Xml('
+            <index>
+                <name>asterisk_sip_peers::context_id--asterisk_context::id</name>
+                <field>
+                    <name>context_id</name>
+                </field>
+                <foreign>true</foreign>
+                <reference>
+                    <table>asterisk_context</table>
+                    <field>id</field>
+                </reference>
+            </index>');
+        $this->_backend->addForeignKey('asterisk_sip_peers', $declaration);       
+        
+        $this->_backend->dropCol('asterisk_sip_peers', 'context');
+        
+        $this->setApplicationVersion('Voipmanager', '2.6');
     }    
 }
