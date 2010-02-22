9bbee076aca43ffb65872c4dba3bbd718faf10ea
Philipp Schüle <p.schuele@metaways.de>
2010-02-22 16:37:15 +0000

    RESOLVED - # 2320: add call forwarding options to sip peer edit dialog http://www.tine20.org/bugtracker/view.php?id=2320 - merged changes from trunk
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2010-03-ms1@12907 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Voipmanager/js/CallForwardPanel.js
@@ -0,0 +1,236 @@
+/*
+ * Tine 2.0
+ * 
+ * @package     Voipmanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+Ext.namespace('Tine.Voipmanager');
+
+/**
+ * Account Picker GridPanel
+ * 
+ * @namespace   Tine.Voipmanager
+ * @class       Tine.Voipmanager.CallForwardPanel
+ * @extends     Ext.form.FormPanel
+ * 
+ * <p>Call Forward Form Panel</p>
+ * <p><pre>
+ * </pre></p>
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ * @param       {Object} config
+ * @constructor
+ * Create a new Tine.Voipmanager.CallForwardPanel
+ */
+Tine.Voipmanager.CallForwardPanel = Ext.extend(Ext.form.FormPanel, {
+
+    /**
+     * @cfg
+     */
+    border: false,
+    frame: true,
+    anchor: '100%',
+                
+    /**
+     * @type Tine.Tinebase.data.Record
+     */
+    record: null,
+    
+    /**
+     * @private
+     */
+    initComponent: function() {
+        this.addEvents(
+            /**
+             * @event change
+             * Fired when user pressed cancel button
+             */
+            'change');
+        this.items = this.getFormItems();
+        
+        Tine.Voipmanager.CallForwardPanel.superclass.initComponent.call(this);
+    },
+
+    /**
+     * returns dialog
+     * 
+     * NOTE: when this method gets called, all initalisation is done.
+     * 
+     * TODO add form items
+     */
+    getFormItems: function() {
+        return [{
+            title: this.app.i18n._('Forward immediately'),
+            autoHeight: true,
+            xtype: 'fieldset',
+            layout: 'form',
+            //labelAlign: 'top',
+            defaults: {
+                anchor: '100%'
+            },
+            items: [{
+                name: 'cfi_mode',
+                xtype: 'combo',
+                fieldLabel: this.app.i18n._('Mode'),
+                typeAhead: true,
+                triggerAction: 'all',
+                lazyRender:true,
+                triggerAction: 'all',
+                allowBlank: false,
+                editable: false,
+                blurOnSelect: true,
+                value: 'off',
+                store: [
+                    ['off', 'off'],
+                    ['number', 'number'],
+                    ['voicemail', 'voicemail']
+                ],
+                listeners: {
+                    scope: this,
+                    select: function(combo, record) {
+                        // disable number if != number
+                        this.getForm().findField('cfi_number').setDisabled(record.data.field1 != 'number');
+                        this.onFieldChange();
+                    }
+                }
+            }, {
+                name: 'cfi_number',    
+                fieldLabel: this.app.i18n._('Number'),
+                xtype: 'textfield',
+                enableKeyEvents: true,
+                listeners: {
+                    keyup: this.onFieldChange,
+                    scope: this
+                }
+            }]
+        }, {
+            title: this.app.i18n._('Forward busy'),
+            autoHeight: true,
+            xtype: 'fieldset',
+            layout: 'form',
+            //labelAlign: 'top',
+            defaults: {
+                anchor: '100%'
+            },
+            items: [{
+                name: 'cfb_mode',
+                xtype: 'combo',
+                fieldLabel: this.app.i18n._('Mode'),
+                typeAhead: true,
+                triggerAction: 'all',
+                lazyRender:true,
+                triggerAction: 'all',
+                allowBlank: false,
+                editable: false,
+                blurOnSelect: true,
+                value: 'off',
+                store: [
+                    ['off', 'off'],
+                    ['number', 'number'],
+                    ['voicemail', 'voicemail']
+                ],
+                listeners: {
+                    scope: this,
+                    select: function(combo, record) {
+                        this.getForm().findField('cfb_number').setDisabled(record.data.field1 != 'number');
+                        this.onFieldChange();
+                    }
+                }
+            }, {
+                name: 'cfb_number',    
+                fieldLabel: this.app.i18n._('Number'),
+                xtype: 'textfield',
+                enableKeyEvents: true,
+                listeners: {
+                    keyup: this.onFieldChange,
+                    scope: this
+                }
+            }]
+         }, {
+            title: this.app.i18n._('Forward delayed'),
+            autoHeight: true,
+            xtype: 'fieldset',
+            layout: 'form',
+            //labelAlign: 'top',
+            defaults: {
+                anchor: '100%'
+            },
+            items: [{
+                name: 'cfd_mode',
+                xtype: 'combo',
+                fieldLabel: this.app.i18n._('Mode'),
+                typeAhead: true,
+                triggerAction: 'all',
+                lazyRender:true,
+                triggerAction: 'all',
+                allowBlank: false,
+                editable: false,
+                blurOnSelect: true,
+                value: 'off',
+                store: [
+                    ['off', 'off'],
+                    ['number', 'number'],
+                    ['voicemail', 'voicemail']
+                ],
+                listeners: {
+                    scope: this,
+                    select: function(combo, record) {
+                        this.getForm().findField('cfd_number').setDisabled(record.data.field1 != 'number');
+                        this.getForm().findField('cfd_time').setDisabled(record.data.field1 == 'off');
+                        this.onFieldChange();
+                    }
+                }
+            }, {
+                name: 'cfd_number',    
+                fieldLabel: this.app.i18n._('Number'),
+                xtype: 'textfield',
+                enableKeyEvents: true,
+                listeners: {
+                    keyup: this.onFieldChange,
+                    scope: this
+                }
+            }, {
+                name: 'cfd_time',      
+                fieldLabel: this.app.i18n._('Delay time'),
+                xtype: 'numberfield',
+                allowNegative: false,
+                value: 30,
+                enableKeyEvents: true,
+                listeners: {
+                    keyup: this.onFieldChange,
+                    scope: this
+                }
+            }]
+        }];
+    },
+    
+    /**
+     * 
+     * @param {Object} record
+     */
+    onRecordLoad: function(record) {
+        this.record = record;
+        
+        this.getForm().findField('cfi_number').setDisabled(record.data.cfi_mode != 'number');
+        this.getForm().findField('cfb_number').setDisabled(record.data.cfb_mode != 'number');
+        this.getForm().findField('cfd_number').setDisabled(record.data.cfd_mode != 'number');
+        this.getForm().findField('cfd_time').setDisabled(record.data.cfd_mode == 'off');
+    },
+
+    /**
+     * fire change event if field changes
+     */
+    onFieldChange: function() {
+        this.fireEvent('change');
+    }
+});
+
